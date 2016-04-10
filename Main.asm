e2c677ee9ef0e02c58e43748209dde873dd299af
hang-on <asje@me.com>
2016-04-10 22:57:56 +0200

    create/destroy obj

--- Main.asm
+++ Main.asm
@@ -16,6 +16,37 @@
   BackgroundPalette:
     .db $35
 
+  SetupMain:
+    ld a,0
+    ld b,1
+    ld hl,BackgroundPalette
+    call LoadCRam
+    ld a,16
+    ld b,SpritePaletteEnd-SpritePalette
+    ld hl,SpritePalette
+    call LoadCRam
+
+    ld hl,SwabbyTiles
+    ld de,$2000
+    ld bc,SwabbyTilesEnd-SwabbyTiles
+    call LoadVRam
+
+    ld a,ENABLE_DISPLAY_ENABLE_FRAME_INTERRUPTS_NORMAL_SPRITES
+    ld b,1
+    call SetRegister
+    ei
+    call AwaitFrameInterrupt
+  jp Main
+.ends
+
+; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+.ramsection "Main variables" slot 3
+  FrameCounter db
+  PlayerObjectHandle db
+.ends
+; -----------------------------------------------------------------------------
+.section "Main" free
+; -----------------------------------------------------------------------------
   SwabbyMetaSprite:
     .db 6
     .db 0 0 0 8 8 8
@@ -25,55 +56,36 @@
     .db 20 40
     .dw SwabbyMetaSprite
 
-  AlternativeSwabbyInitString:
-    .db 100 150
-    .dw SwabbyMetaSprite
-
-
-SetupMain:
-  ld a,0
-  ld b,1
-  ld hl,BackgroundPalette
-  call LoadCRam
-  ld a,16
-  ld b,SpritePaletteEnd-SpritePalette
-  ld hl,SpritePalette
-  call LoadCRam
-
-  ld hl,SwabbyTiles
-  ld de,$2000
-  ld bc,SwabbyTilesEnd-SwabbyTiles
-  call LoadVRam
-
-  ld hl,SwabbyInitString
-  call CreateObject
-  ld hl,AlternativeSwabbyInitString
-  call CreateObject
-
-  ld a,ENABLE_DISPLAY_ENABLE_FRAME_INTERRUPTS_NORMAL_SPRITES
-  ld b,1
-  call SetRegister
-  ei
-  call AwaitFrameInterrupt
-  jp Main
-.ends
-
-; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-.ramsection "Main variables" slot 3
-  FrameCounter db
-.ends
-; -----------------------------------------------------------------------------
-.section "Main" free
-; -----------------------------------------------------------------------------
   Main:
     call AwaitFrameInterrupt
     call LoadSAT
 
-    call GetInputPorts
-    call ObjectFrame
-
+    ld a,(FrameCounter)
+    cp 0
+    jp nz,+
+      ld hl,SwabbyInitString
+      call CreateObject
+      call c,_ObjectOverflow
+      ld (PlayerObjectHandle),a
+      jp ++
+    +:
+    cp 127
+    jp nz,++
+      ld a,(PlayerObjectHandle)
+      call DestroyObject
+    ++:
     ld hl,FrameCounter
     inc (hl)
+
+    call GetInputPorts
+
+    call ObjectFrame
+
   jp Main
+
+  _ObjectOverflow:
+    ; Trap error.
+    nop
+  jp _ObjectOverflow
 
 .ends
