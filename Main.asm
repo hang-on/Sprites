9bbcdee26fb94281c73d7905f1fadc2fd8f36786
hang-on <asje@me.com>
2016-04-13 12:45:05 +0200

    Super data driven batchloading blazing...

--- Main.asm
+++ Main.asm
@@ -13,18 +13,40 @@
 ; -----------------------------------------------------------------------------
 .section "Main" free
 ; -----------------------------------------------------------------------------
-  SpritePalette:
-    .include "Data/Sprite-Palette.inc"
-  SpritePaletteEnd:
+  Palette:
+    BackgroundPalette:
+      .db $35
+      .ds 15 0
+    SpritePalette:
+      .include "Data/Sprite-Palette.inc"
+    SpritePaletteEnd:
 
-  BackgroundPalette:
-    .db $35
+  ; Tilebank 2 map:
+  .equ PLAYER_TILES_START $2000
+  .equ ENEMY_1_TILES_START $2400
+  .equ ENEMY_2_TILES_START $2600
 
-  SpriteTilesBegin:
+  ; Make character code indexes for the meta sprite data blocks.
+  .equ P1 (PLAYER_TILES_START-$2000)/32
+  .equ E1 (ENEMY_1_TILES_START-$2000)/32
+  .equ E2 (ENEMY_2_TILES_START-$2000)/32
+
+  SwabbyTiles:
     .include "Data/Swabby-Tiles.inc"
+  SwabbyTilesEnd:
+  GargoyleTiles:
     .include "Data/Gargoyle-Tiles.inc"
+  GargoyleTilesEnd:
+  ZombieTiles:
     .include "Data/Zombie-Tiles.inc"
-  SpriteTilesEnd:
+  ZombieTilesEnd:
+
+  BatchLoadTable:
+    .db (BatchLoadTableEnd-BatchLoadTable-1)/6
+    .dw SwabbyTiles PLAYER_TILES_START SwabbyTilesEnd-SwabbyTiles
+    .dw GargoyleTiles ENEMY_1_TILES_START GargoyleTilesEnd-GargoyleTiles
+    .dw ZombieTiles ENEMY_2_TILES_START ZombieTilesEnd-ZombieTiles
+  BatchLoadTableEnd:
 
   SwabbyInitString:
     .db 1                     ; Initial status.
@@ -34,15 +56,15 @@
   SwabbyFlying1:
     .db 6
     .db -4, -4, -4, 4, 4, 4
-    .db -8, 0, 0, 1, 8, 2, -8, 3, 0, 4, 8, 5
+    .db -8, P1, 0, P1+1, 8, P1+2, -8, P1+3, 0, P1+4, 8, P1+5
   SwabbyFlying2:
     .db 6
     .db -4, -4, -4, 4, 4, 4
-    .db -8, 6, 0, 7, 8, 8, -8, 9, 0, 10, 8, 11
+    .db -8, P1+6, 0, P1+7, 8, P1+8, -8, P1+9, 0, P1+10, 8, P1+11
   SwabbyFlying3:
     .db 6
     .db -4, -4, -4, 4, 4, 4
-    .db -8, 12, 0, 13, 8, 14, -8, 15, 0, 16, 8, 17
+    .db -8, P1+12, 0, P1+13, 8, P1+14, -8, P1+15, 0, P1+16, 8, P1+17
 
   GargoyleInitString:
     .db 1
@@ -52,7 +74,7 @@
   GargoyleMetaSprite:
     .db 4
     .db -4, -4, 4, 4
-    .db -4, 18, 4, 19, -4, 20, 4, 21
+    .db -4, E1, 4, E1+1, -4, E1+2, 4, E1+3
 
   ZombieInitString:
     .db 1
@@ -62,8 +84,7 @@
   ZombieMetaSprite:
     .db 6
     .db -8, -8, 0, 0, 8, 8
-    .db -4, 26, 4, 27, -4, 28, 4, 29, -4, 30, 4, 31
-
+    .db -4, E2, 4, E2+1, -4, E2+2, 4, E2+3, -4, E2+4, 4, E2+5
 
   _EventTable:
     .dw _Event0 _Event1 _Event2
@@ -87,18 +108,12 @@
 
   SetupMain:
     ld a,0
-    ld b,1
-    ld hl,BackgroundPalette
-    call LoadCRam
-    ld a,16
-    ld b,SpritePaletteEnd-SpritePalette
-    ld hl,SpritePalette
+    ld b,32
+    ld hl,Palette
     call LoadCRam
 
-    ld hl,SpriteTilesBegin
-    ld de,$2000
-    ld bc,SpriteTilesEnd-SpriteTilesBegin
-    call LoadVRam
+    ld hl,BatchLoadTable
+    call _BatchLoadTiles
 
     ld a,ENABLE_DISPLAY_ENABLE_FRAME_INTERRUPTS_NORMAL_SPRITES
     ld b,1
@@ -122,6 +137,27 @@
     call ObjectFrame
 
   jp Main
+
+  _BatchLoadTiles:
+    ; Entry: HL = Base address of BatchLoadTable to use
+    ld b,(hl)             ; Read number of entries in the BatchLoadTable.
+    inc hl
+    push hl
+    pop ix
+    -:
+      push bc
+        ld l,(ix+0)       ; Get pointer to tile data.
+        ld h,(ix+1)
+        ld e,(ix+2)       ; Get pointer to destination in vram.
+        ld d,(ix+3)
+        ld c,(ix+4)       ; Get number of bytes to load.
+        ld b,(ix+5)
+        call LoadVRam
+        ld de,6
+        add ix,de         ; Advance the BatchLoadTable index to next entry.
+      pop bc
+    djnz -                ; Process all elements in BatchLoadTable
+  ret
 
   _MakeEvent:
     ld a,(NextEventIndex)
